d96e88a56e4e3a44ef2596a17e10186b531196cf
Sakshiflix <sakshi01998@gmail.com>
2026-09-15 15:57:48 +0530

    Functions

---
+++ program3.js
@@ -0,0 +1,30 @@
+// ============================================================
+// PROGRAM 3: FUNCTIONS
+// Parameters, Arguments & Return
+// ============================================================
+
+console.log("\n========== PROGRAM 3 ==========");
+
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+let result1 = add(10, 5);
+let result2 = multiply(10, 5);
+
+console.log("Addition:", result1);
+console.log("Multiplication:", result2);
+console.log("Subtraction:", subtract(10, 5));
+console.log("Division:", divide(10, 5));
