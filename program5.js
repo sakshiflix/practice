1a985c04dda2246bd8f64d890f1b13b3cbdfbbc0
Sakshiflix <sakshi01998@gmail.com>
2026-09-15 16:43:44 +0530

    For Loop

---
+++ program5.js
@@ -0,0 +1,20 @@
+// ============================================================
+// PROGRAM 5: FOR LOOP
+// ============================================================
+
+console.log("\n========== PROGRAM 5 ==========");
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// Print even numbers
+
+console.log("Even numbers:");
+
+for (let i = 1; i <= 20; i++) {
+
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
